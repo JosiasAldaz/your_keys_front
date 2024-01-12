629b3af2a7f108857e7583d30871dccd607d8d9d
JosiasAldaz <josiasjoestar@hotmail.com>
2024-01-12 12:06:48 -0500

    creación del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AutoComponent } from './components/auto/auto.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AutoComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
